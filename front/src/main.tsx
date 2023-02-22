bf56ba745af5b988897af047023d5cead7c44a50
Jonas <jonathan.gonzalez@gm2dev.com>
2023-02-22 19:47:00 -0300

    feat(app): initialized project and config tailwind

---
+++ front/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { SystemRoutes } from './modules/routes';
+import { store } from '../redux/store/index';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <SystemRoutes />
+    </Provider>
+  </React.StrictMode>
+);

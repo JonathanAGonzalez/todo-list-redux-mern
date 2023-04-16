2542f4720c5ad41e38d2578e7ba2520059f31f51
Jona <jonathang@hotmail.com.ar>
2023-04-16 12:23:02 -0300

    feat(main):add toastify css

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { SystemRoutes } from './modules/routes';
 import { store } from '../redux/store/index';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
